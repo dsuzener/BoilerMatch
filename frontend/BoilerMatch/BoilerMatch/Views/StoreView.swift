4b44f7a0312bfd33c286842ba0c6cfaa21bb9c27
dsuzener <141687334+dsuzener@users.noreply.github.com>
2025-02-22 17:00:29 -0500

    Create StoreView.swift

---
+++ frontend/BoilerMatch/BoilerMatch/Views/StoreView.swift
@@ -0,0 +1,19 @@
+//
+//  StoreView.swift
+//  BoilerMatch
+//
+//  Created by Omniscient on 2/22/25.
+//
+
+import SwiftUI
+
+struct StoreView: View {
+    var body: some View {
+        Text("Store View")
+            // Placeholder for Store functionality (e.g., buying hammers or watching videos for gems)
+    }
+}
+
+#Preview {
+    StoreView()
+}
