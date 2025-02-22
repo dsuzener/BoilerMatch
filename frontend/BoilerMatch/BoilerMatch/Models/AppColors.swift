09ebcfa1b4e7937edc3c4a682526cbcba80f8918
dsuzener <141687334+dsuzener@users.noreply.github.com>
2025-02-22 15:27:20 -0500

    Update AppColors.swift
    - New Purdue colors

--- frontend/BoilerMatch/BoilerMatch/Models/AppColors.swift
+++ frontend/BoilerMatch/BoilerMatch/Models/AppColors.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct AppColors {
+    // App logo colors
     static let lightBeige = Color(hex: 0xE6D0B1)
     static let mediumBeige = Color(hex: 0xF1D9AD)
     static let darkBeige = Color(hex: 0xDEC597)
     static let lightestBeige = Color(hex: 0xFFEAD1)
+    
+    // Purdue branding colors
+    static let boilermakerGold = Color(hex: 0xCFB991) // Primary gold
+    static let black = Color(hex: 0x000000)          // Primary black
+    static let agedGold = Color(hex: 0x8E6F3E)       // Supporting gold
+    static let rushGold = Color(hex: 0xDAAA00)       // Vibrant gold
+    static let coolGray = Color(hex: 0x6F727B)       // Supporting gray
 }
 
 extension Color {
