d8a8708ddfe398f5c488312d6a1b8e3e62daf27a
dsuzener <141687334+dsuzener@users.noreply.github.com>
2025-02-23 00:59:43 -0500

    Update FeedItem.swift

--- frontend/BoilerMatch/BoilerMatch/Models/FeedItem.swift
+++ frontend/BoilerMatch/BoilerMatch/Models/FeedItem.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-struct FeedItem: Identifiable {
+struct FeedItem: Identifiable, Decodable {
     let id = UUID()
     let name: String
     let age: Int
     let imageName: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case name, age, imageName
+    }
 }
